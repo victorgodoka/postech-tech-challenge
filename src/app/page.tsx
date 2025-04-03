b046ce8d980350e3705a228973d88b451991c3f6
Victor Martins <martinsvictorsantos@gmail.com>
2025-04-03 09:44:26 -0300

    feat: criação da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,64 @@
+import { Button } from "@/components/Button";
+import { FeatureCard } from "@/components/FeatureCard";
+import { actionsBlack } from "@/const";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="bg-gradient-to-b from-green-from to-green-to px-4 py-8">
+      <div className="flex flex-col xl:flex-row items-center justify-center gap-8">
+        <p className="text-center xl:text-left text-black text-2xl font-bold w-full max-w-[446px]">
+          Experimente mais liberdade no controle da sua vida financeira.<br />
+          Crie sua conta com a gente!
+        </p>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"/home-graphs.png"}
+          width={661}
+          height={412}
+          className="my-8 w-full max-w-78 md:max-w-[600px] xl:max-w-[661px]"
+          alt={"Gráfico na Home"}
+          sizes="(max-width: 767px) 312px, (max-width: 1279px) 600px, 661px"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </div>
+      <div className="flex md:hidden gap-4 items-center justify-center py-4">
+        {actionsBlack.map((action, index) => (
+          <Button type="a" href="#" variant={action.variant} key={index}>
+            {action.label}
+          </Button>
+        ))}
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="flex flex-wrap max-w-3xl mx-auto text-center justify-center">
+        <h2 className="w-full font-bold text-2xl">Vantagens do nosso banco:</h2>
+        <FeatureCard
+          icon={"mynaui:gift"}
+          title={"Conta e cartão gratuitos"}
+          description={
+            "Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção."
+          }
+        />
+        <FeatureCard
+          icon={"majesticons:money-hand"}
+          title={"Saques sem custo"}
+          description={
+            "Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h."
+          }
+        />
+        <FeatureCard
+          icon={"meteor-icons:star"}
+          title={"Programa de pontos"}
+          description={
+            "Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!"
+          }
+        />
+        <FeatureCard
+          icon={"tdesign:device"}
+          title={"Seguro Dispositivos"}
+          description={
+            "Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica."
+          }
+        />
+      </div>
     </div>
   );
 }
