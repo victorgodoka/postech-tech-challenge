5ed9d72aba96d270ffecbbbf999608a74702f47f
Victor Martins <martinsvictorsantos@gmail.com>
2025-08-03 18:05:58 -0300

    feat: configured mf enviroment

---
+++ apps/dashboard/next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
+  experimental: {
+    esmExternals: true,
+  },
+}
+
+module.exports = nextConfig
